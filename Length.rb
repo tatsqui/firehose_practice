d1e323b4ec47313202bb10c78fddcdf5fb9555c3
tatsqui <34796138+tatsqui@users.noreply.github.com>
2018-02-01 18:58:01 -0500

    Create Length.rb

---
+++ Length.rb
@@ -0,0 +1,11 @@
+# Lesson 20 math & numbers 
+# Methods, interpolation, concatenation, floats, integers, strings
+#------------------------------------------
+
+
+puts "What is your height (in inches)?"
+    height = gets.chomp.to_f
+
+    cm = height.to_f * 2.54
+
+puts "Your height in centimeters is #{cm}cm."
