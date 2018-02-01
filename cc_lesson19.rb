9bb2b5cd8239086f48f8e768e07befeb7da73e00
tatsqui <34796138+tatsqui@users.noreply.github.com>
2018-02-01 18:57:08 -0500

    Create cc_lesson19.rb

---
+++ cc_lesson19.rb
@@ -0,0 +1,10 @@
+puts "Welcome to Goodburger, home of the Goodburger!"
+
+puts "What's your name?"
+name = gets.chomp
+
+puts "Today we have two items on the menu.
+Would you like a Goodburger or a milkshake?"
+food = gets.chomp
+
+puts "Thank you #{name}, your #{food} will be right out!"
