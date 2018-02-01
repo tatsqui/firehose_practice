bd54880ea61d447e7b2a5cd5c87191c401f94ea1
tatsqui <34796138+tatsqui@users.noreply.github.com>
2018-02-01 18:59:24 -0500

    Create input.rb

---
+++ input.rb
@@ -0,0 +1,11 @@
+# Lesson 19 in Firehose Intro Course
+
+puts "Enter your name:"
+name = gets.chomp
+
+puts "Enter something you own:"
+own = gets.chomp
+
+greeting = "Hello #{name}. What an excellent #{own} you have!"
+
+puts greeting
